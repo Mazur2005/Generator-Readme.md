38cc14ee3c796df4e0e2974588a9faecd4173831
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-03-21 15:13:40 +0100

    reduction of names

--- src/services/redux/store.tsx
+++ src/services/redux/store.tsx
@@ -1,21 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducerDragDrop from "./actions/isDragDrop";
-import changeEditor from "./actions/editorOrder";
-import selectedOption from "./actions/pickOption";
+import isDragDrop from "./actions/isDragDrop";
+import editorOrder from "./actions/editorOrder";
+import pickOption from "./actions/pickOption";
 import createSketch from "./actions/createSketch";
 import isDesktop from "@/services/redux/actions/isDesktop";
 
 export const store = configureStore({
 	reducer: {
-		isDragDrop: reducerDragDrop,
-		editorOrder: changeEditor,
-		pickOption: selectedOption,
-		createSketch: createSketch,
+		isDragDrop,
+		editorOrder,
+		pickOption,
+		createSketch,
 		isDesktop,
 	},
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
